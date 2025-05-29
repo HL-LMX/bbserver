65c7bdfff39c550575b3aa22f1227d0506fe06c7
Shelem H. <shelemhg@hotmail.com>
2025-05-29 11:08:15 -0600

    fix: Remove mixed files from frontend

--- src/Variables.js
+++
@@ -1,7 +0,0 @@
-// client/bookinggite/src/Variables.js
-
-export const variables={
-    // API_URL:"http://127.0.0.1:8000/",
-    API_URL:"http://ec2-13-58-236-3.us-east-2.compute.amazonaws.com/",
-    
-}
