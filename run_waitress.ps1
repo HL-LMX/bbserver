3e797d15395383e1bfbe772a1fa3dfa0071e57c7
shelemhl <146783144+shelemhl@users.noreply.github.com>
2025-09-02 15:35:01 -0600

    ci: Update command from alias to the actual Powershell instruction

--- run_waitress.ps1
+++ run_waitress.ps1
@@ -1,6 +1,7 @@
 # deployments/bbserver/run_waitress.ps1
+
 # PowerShell script to run Django via Waitress
 $env:DJANGO_SETTINGS_MODULE = "bookingbite.settings"
 $env:SERVERNAMES = "localhost,127.0.0.1,194.9.161.245"
-cd "C:\deployments\bbserver"
+Set-Location "C:\deployments\bbserver"
 .\venv\Scripts\python.exe -m waitress --listen=127.0.0.1:8000 bookingbite.wsgi:application
